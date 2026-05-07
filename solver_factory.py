1bae5c2ad81193db37a4f8a2630df78f4b00e498
Tomas <164687684+r4vaitis@users.noreply.github.com>
2026-05-07 19:27:52 +0300

    Delete solver_factory.py

--- solver_factory.py
+++
@@ -1,9 +0,0 @@
-from solver import BacktrackingSolver
-
-
-class SolverFactory:
-    def create_solver(self, solver_type):
-        if solver_type == "backtracking":
-            return BacktrackingSolver()
-
-        return None
